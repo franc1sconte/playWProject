d91f60add9d1a2e23f09145226d983bc259fe62c
Francisco Conte <francisco@duxsoftware.com.ar>
2025-06-08 22:45:45 -0300

    QA-1 terminado

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,40 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Grupo de pruebas - Heroku', () => {
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  test('QA-1 - A/B Testing', async ({ page }) => {
+    const linkTitle = 'A/B Testing';
+    const testTitle = 'A/B Test Control';
+    const url_homepage = 'https://the-internet.herokuapp.com/'
+    const url_test = 'https://the-internet.herokuapp.com/abtest'
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+    // --> Función para obtener el link por nombre
+    function obtenerLinkPorNombre(nombreLink: string) {
+      return page.locator(`[id=content] ul li:has-text("${nombreLink}") a`);
+    }
+
+    await test.step("Ingresamos a heroku", async () => {
+      const heading = page.locator('h1[class="heading"]');
+      
+      await page.goto(url_homepage);
+      await expect(page).toHaveURL(url_homepage);
+      await expect(heading).toBeVisible();
+      await expect(heading).toHaveText('Welcome to the-internet');
+    })
+
+    await test.step("Clickeamos en la primer seccion [A/B Testing]", async () => {
+      const divPadre = page.locator('[class="example"]');
+      const titulo = divPadre.locator('h3');
+      
+      await obtenerLinkPorNombre(linkTitle).click();
+      await expect(page).toHaveURL(url_test);
+      await expect(titulo).toHaveText(testTitle)
+    })
+
+  })
+  
+
+
 });
